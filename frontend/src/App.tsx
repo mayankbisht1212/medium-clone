64419e5790858c1bdb8e9078fa49678ebcb55f28
Mayank Bisht <mayankbishtdec12@gmail.com>
2026-07-07 00:54:47 +0530

    changed the default page redirect to signup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Signup/>}/> 
           <Route path="/Signup" element={<Signup/>}/> 
           <Route path="/Signin" element={<Signin/>}/> 
           <Route path="/blog/:id" element={<Blog/>}/> 
